2e7abdd0dbd7148037e0c8f62827f5ca6962dfc5
shyam <shyamkachhadiya4@gmail.com>
2024-12-08 22:26:41 +0530

    Initial commit

---
+++ pro.js
@@ -0,0 +1,9 @@
+// Change Navbar Background on Scroll
+window.addEventListener('scroll', () => {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
